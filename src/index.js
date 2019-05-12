80f433a582aaccc96cbb888d8d1b7d4d43096901
Jennifer Monsalve <JMonsalve@student.neumont.edu>
2019-05-12 15:24:58 -0600

    Pre-build click work
    Just need to connected to other function for it to work

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ console.log("Hello");
 let preBuiltID = ["pre1", "pre2","pre3","pre4","pre5"]
 
 function setEventListener(){
-   preBuiltID.forEach((id) => {
-       document.getElementById(id).addEventListener("click", PreBuildClick(id));
-   })
-}
-function PreBuildClick(name) {
-    document.getElementById("pre1").innerHTML = "YOU CLICKED ME!";
+    preBuiltID.forEach((id) => {
+        document.getElementById(id).addEventListener("click", function(){
+            
+        });
+    })
 }
 setEventListener();
