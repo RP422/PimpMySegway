2af33486f7fb4db6c11a24e07bc464f2ec24de3f
Jennifer Monsalve <JMonsalve@student.neumont.edu>
2019-05-12 16:59:59 -0600

    This should work
    ...Hopefully

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ let preBuiltID = ["princess", "big_boi","sad_lad","lightning_mcqueen","explorer"
 function setPrebuildEventListener(){
     preBuiltID.forEach((id) => {
         document.getElementById(id).addEventListener("click", function(){
-            setPrebuilt();
+            setPrebuilt(id);
         });
     })
 }
